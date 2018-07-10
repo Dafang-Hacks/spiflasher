3984e69bcec8bf3e89eb6e64848fdd4a0ce37b44
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-10 19:36:55 +0200

    SPI Flasher

--- read.py
+++
@@ -1,4 +0,0 @@
-from GD25Q128 import GD25Q128Reader
-flasher = GD25Q128Reader()
-flasher.connect("/dev/ttyUSB0")
-flasher.dump_rom("flash.bin")
--- write.py
+++ write.py
@@ -1,4 +1,6 @@
 from GD25Q128 import GD25Q128Reader
 flasher = GD25Q128Reader()
 flasher.connect("/dev/ttyUSB0")
+flasher.parse_SR()
+flasher.read_id()
 flasher.write_rom("fullflash.bin")
